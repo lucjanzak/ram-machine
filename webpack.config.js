ddc020f45bb8f40444c29d04cd0c31a33f0f7ee2
Lucjan Żak <lucjanzak@gmail.com>
2026-03-31 13:10:01 +0200

    Add git commit hash to version number

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,23 @@ import MonacoWebpackPlugin from "monaco-editor-webpack-plugin";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import CopyWebpackPlugin from "copy-webpack-plugin";
 import packageJson from "./package.json" with { type: "json" };
+import process from "node:process";
+
+const packageVersion = packageJson.version;
+const gitCommitHash = process.env.GIT_COMMIT_HASH;
+if (gitCommitHash === undefined) {
+  throw new Error("GIT_COMMIT_HASH environment variable not provided; please provide a valid value for GIT_COMMIT_HASH, or set it to an empty string")
+}
+
+const gitCommitHashShort = gitCommitHash.substring(0, 7);
+const displayedVersion = ((packageVersion.includes("alpha") || packageVersion.includes("beta")) && gitCommitHash !== "") ? `${packageVersion}-${gitCommitHashShort}` : packageVersion;
+const templateParameters = {
+  packageVersion,
+  gitCommitHash,
+  gitCommitHashShort,
+  displayedVersion
+};
+console.log("Creating template with parameters: ", templateParameters);
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -54,9 +71,7 @@ const config /*: webpack.Configuration*/ = {
     new HtmlWebpackPlugin({
       title: "Development",
       template: "./src/index.html",
-      templateParameters: {
-        packageVersion: packageJson.version,
-      },
+      templateParameters,
     }),
     new MonacoWebpackPlugin(),
     new CopyWebpackPlugin({
